0006065478a1633293249da7820ad8846415e313
Alex Marin Mendez <alexmarinmendez@gmail.com>
2022-02-18 09:49:40 -0500

    feat: Reference access

--- src/utils/firestoreFetch.js
+++ src/utils/firestoreFetch.js
@@ -5,7 +5,11 @@ import db from './firebaseConfig';
 export const firestoreFetch = async (idCategory) => {
     let q;
     if (idCategory) {
-        q = query(collection(db, "products"), where('categoryId', '==', idCategory));
+        const categoryDocRef = doc(db, "categories", idCategory);
+        q = query(
+            collection(db, "products"),
+            where("category", "==", categoryDocRef)
+        );
     } else {
         q = query(collection(db, "products"));
     }
@@ -22,10 +26,12 @@ export const firestoreFetchOne = async (idItem) => {
     const docSnap = await getDoc(docRef);
     
     if (docSnap.exists()) {
-      return {
-          id: idItem,
-          ...docSnap.data()
+      let result = {
+        id: idItem,
+        ...docSnap.data()
       }
+    //   console.log(result);
+      return result;
     } else {
       // doc.data() will be undefined in this case
       console.log("No such document!");
